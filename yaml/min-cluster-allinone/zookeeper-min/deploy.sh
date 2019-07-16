0b3f6425a1c2a8e9ad21facb06065c1a805271ef
hepengyuan <hpy253215039@163.com>
2019-07-16 17:12:57 +0800

    add consul

--- yaml/min-cluster-allinone/zookeeper-min/deploy.sh
+++ yaml/min-cluster-allinone/zookeeper-min/deploy.sh
@@ -1,2 +1,4 @@
 kubectl apply -f zookeeper-min-statefulset.yaml
 kubectl apply -f zookeeper-min-pv.yaml
+
+#kubectl delete pv zookeeper-min-data0-pv-local zookeeper-min-data1-pv-local  zookeeper-min-data2-pv-local
