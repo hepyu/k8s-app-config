8052e6630517861fc609b50e26cbd5db2cad5ce8
hepengyuan <hpy253215039@163.com>
2019-09-01 21:20:28 +0800

    update

--- yaml/min-cluster-allinone/kibana-min/deploy.sh
+++ yaml/min-cluster-allinone/kibana-min/deploy.sh
@@ -1,3 +1,3 @@
 kubectl create namespace es-min-kibana
-kubectl apply -f kibana-min-deployment.yaml
-kubectl apply -f kibana-min-service.yaml
+kubectl apply -f kibana-deployment.yaml
+kubectl apply -f kibana-service.yaml
