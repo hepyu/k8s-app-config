179a378d07b042ecaff0089fe1108cc10db042ba
hepengyuan <hpy253215039@163.com>
2019-11-22 21:35:29 +0800

    update

--- yaml/min-cluster-allinone/apollo-min/deploy.sh
+++ yaml/min-cluster-allinone/apollo-min/deploy.sh
@@ -2,5 +2,5 @@ kubectl create namespace inc
 kubectl apply -f service-apollo-admin-server-dev.yaml
 kubectl apply -f service-apollo-config-server-dev.yaml
 kubectl apply -f service-apollo-portal-server.yaml
-kubectl apply -f apollo-configservice-transition-service.yaml
-kubectl apply -f apollo-configservice-transition-ingress.yaml
+kubectl apply -f apollo-configservice-transition-service-dev.yaml
+kubectl apply -f apollo-configservice-transition-ingress-dev.yaml
