d97ce89f2a2400db3735d6ae189e9f9614a3d5c7
hpy <hpy253215039@163.com>
2021-02-04 20:25:06 +0800

    add tekton dashboard

---
+++ yaml/min-cluster-allinone/tekton/dashboard/make-and-push-private-image.sh
@@ -0,0 +1,3 @@
+docker pull gcr.io/tekton-releases/github.com/tektoncd/dashboard/cmd/dashboard
+docker tag gcr.io/tekton-releases/github.com/tektoncd/dashboard/cmd/dashboard hpy253215039/tektoncd-dashboard-cmd-dashboard
+docker push hpy253215039/tektoncd-dashboard-cmd-dashboard
