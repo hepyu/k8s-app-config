5230093cbd892635b967a2d2a3340d8bb967d14e
hpy <hpy253215039@163.com>
2021-02-04 20:27:02 +0800

    update

---
+++ yaml/min-cluster-allinone/tekton/pipeline/push-to-hub.sh
@@ -0,0 +1,11 @@
+docker push hpy253215039/tektoncd-pipeline-cmd-controller:v0.20.1
+docker push hpy253215039/tektoncd-pipeline-cmd-kubeconfigwriter:v0.20.1
+docker push hpy253215039/tektoncd-pipeline-cmd-git-init:v0.20.1
+docker push hpy253215039/tektoncd-pipeline-cmd-entrypoint:v0.20.1
+docker push hpy253215039/tektoncd-pipeline-cmd-nop:v0.20.1
+docker push hpy253215039/tektoncd-pipeline-cmd-imagedigestexporter:v0.20.1
+docker push hpy253215039/tektoncd-pipeline-cmd-pullrequest-init:v0.20.1
+docker push hpy253215039/tektoncd-pipeline-vendor-googlecloudplatform-cloud-builders-gcs-fetcher:v0.20.1
+docker push hpy253215039/google-cloudsdktool-cloud-sdk
+docker push hpy253215039/distroless-base
+docker push hpy253215039/tektoncd-pipeline-cmd-webhook:v0.20.1
